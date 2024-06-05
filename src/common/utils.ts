c9932aa4ef45f3a7323bf01a346b64008a6ea494
saddxni <dgarciacasam@gmail.com>
2024-06-05 18:52:20 +0200

    Cambiada función para convertir fotos de perfil al archivo utils

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -25,3 +25,27 @@ export function getCards (projects: types.Project[]): types.Task[][] {
 export function getCard (columnId: number, projects: types.Project[]): types.Task[] {
   return projects.flatMap(project => project.tasks.filter(task => task.columnId === columnId))
 }
+
+// Función que convierte la imagen en base64 en un blob que utilizar como imagen
+export const convertProfilePic = (base64Image: string): string => {
+  const decodedImage = atob(base64Image)
+  const ab = new ArrayBuffer(decodedImage.length)
+  const ia = new Uint8Array(ab)
+  for (let i = 0; i < decodedImage.length; i++) {
+    ia[i] = decodedImage.charCodeAt(i)
+  }
+  const blob = new Blob([ab], { type: 'image/jpeg' })
+  const imageUrl = window.URL.createObjectURL(blob)
+  return imageUrl
+}
+
+export function getUsersData (users: types.Users[]) {
+  const dataAvatar = users.map((usuario: types.User) => ({
+    id: usuario.id,
+    name: usuario.name ?? 'Nombre',
+    designation: usuario.email ?? 'Email',
+    image: convertProfilePic(usuario.profilePic)
+  }))
+
+  return dataAvatar
+}
